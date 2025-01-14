2747acdb34bfd00d87411910c637ad421be496c2
pea5e <sindibade2011@gmail.com>
2025-01-14 03:45:18 +0100

    final commit

--- script.js
+++ script.js
@@ -21,7 +21,8 @@ document.body.onload = () => {
   
           this.x = x;
           this.y = y;
-          this.x_dir = x_dir;
+          // this.x_dir = x_dir;
+          this.x_dir = 0;
           this.y_dir = y_dir;
           this.size = size;
           this.opacity = opacity; 
@@ -103,36 +104,30 @@ document.body.onload = () => {
             // last_node = n.current_pos();
           
         });
-      },100);
+      },50);
 
       const game = document.getElementsByClassName("game")[0]
       const button = document.getElementsByClassName("button")[0] 
       const quoteBlock = document.getElementsByTagName("blockquote")[0]
+      const timer = document.getElementsByClassName("timer")[0]
 
-      const replacments = 8;
+
+      const replacments = 6;
 
       const run = () => {
         var author;
         var quote;
         var html;
         var replacment = []
-        var style = document.createElement('style');
-        for(let i=0;i<replacments;i++){
-            do{
-                var rand = Math.random() * 26 | 0;
-            }while(replacment.includes(rand));
-            replacment.push(rand)
-            style.innerHTML += `
-                .ind-${i}:before {
-                    content: "${i+1}";
-                    position :relative;
-                    top:4vh;
-                    font-space : 
-                }
-            `;
-        }      
-        document.head.appendChild(style);
-        console.log(replacment)
+
+        var answers = new Array(replacments);
+
+        const selectInd = (event) => {
+            Array.from(document.getElementsByClassName("selectedInd")).forEach(e=>e.classList.remove("selectedInd"))
+            Array.from(document.getElementsByClassName(event.currentTarget.classList.item(1))).forEach(e=>e.classList.add("selectedInd"))
+        }
+        
+        // console.log(replacment)
         button.style.visibility = "hidden"
         quoteBlock.style.display = "none"
         fetch("https://salimking.pythonanywhere.com/api/quotes/random").then(r=>r.json()).then(r=>{
@@ -140,14 +135,79 @@ document.body.onload = () => {
                 author = r[0].a
                 quote = r[0].q.toLowerCase();
                 html = r[0].h
+
+                var style = document.createElement('style');
+                for(let i=0;i<replacments;i++){
+                    do{
+                        var rand = Math.random() * 26 | 0;
+                    }while(replacment.includes(rand) || ! quote.includes(String.fromCharCode(rand+97)));
+                    replacment.push(rand)
+                    style.innerHTML += `
+                        .ind-${i}:before {
+                            content: "${i+1}";
+                            position :relative;
+                            top:4vh;
+                            /*font-space : */
+                        }
+                    `;
+                } 
+                
+                
+                document.head.appendChild(style);
+
                 for(let index=0;index<quote.length;index++)
                 {
                     let ascii = replacment.indexOf(quote.charCodeAt(index)-97)
                     if(ascii!=-1)
+                    {
                         game.innerHTML += `<strong class="crypted ind-${ascii}"></strong>`
+                        // let crypted = document.getElementsByClassName("crypted")
+                        // console.log(crypted[crypted.length-1])
+                        // crypted[crypted.length-1].addEventListener("click",selectInd)
+                    }
                     else
                         game.innerHTML += quote.charAt(index)
                 }
+
+                Array.from(document.getElementsByTagName("strong")).forEach(e=>e.addEventListener("click",selectInd))
+
+                timer.innerHTML = "0"
+
+                timerInt = setInterval(()=>{
+                  timer.innerHTML = parseInt(timer.innerHTML)+1 
+                },1000)
+
+                document.addEventListener("keydown", event =>{
+                  
+                  var key = event.key.toLowerCase()
+                  var selectInd = document.getElementsByClassName("selectedInd")
+                  console.log(replacment)
+                  console.log(answers)
+                  if(selectInd.length>0 && key.length==1 && key.charCodeAt(0)>=97 && key.charCodeAt(0)<=97+25 )
+                  {
+                    Array.from(selectInd).forEach(e=>e.innerHTML=`<small>${key}</small>`)
+                    answers[selectInd[0].classList.item(1).split('-')[1]] = key.charCodeAt(0)-97
+                    if(!answers.includes(undefined))
+                    {
+                      answers.forEach((v,e)=>{
+                        if(v != replacment[e])
+                          return;
+                      });
+                      replacment = []
+                      answers = [] 
+                      setTimeout(()=>{
+                        quoteBlock.innerHTML = html
+                        game.innerHTML = ''
+                        button.style.visibility = "visible"
+                        quoteBlock.style.display = "block"
+                        clearInterval(timerInt)
+                        timer.innerHTML = ''
+                      },2000) 
+                      
+                    }
+                  }
+                });
+
             })
         }
     
